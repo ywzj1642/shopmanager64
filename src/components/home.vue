b66673be962963939f2feb3f881c76392fe107c2
ywzj1642 <1642763705@qq.com>
2019-02-16 11:54:54 +0800

    添加容器

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>home----</div>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
-
 <style>
-
+    .header {
+        background: #aaa;
+    }
+    .container {
+        height: 100%;
+    }
+    .aside {
+        background: pink;
+    }
+    .main {
+        background: greenyellow;
+    }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,81 +1,103 @@
 <template>
-    <div class="login-wrap">
-        <el-form class="login-form" :label-position="labelPosition" label-width="80px" :model="formdata">
-            <h2>用户登录</h2>
-            <el-form-item label="用户名">
-            <el-input v-model="formdata.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-            <el-input v-model="formdata.password"></el-input>
-            </el-form-item>
-            <el-button 
-            @click.prevent="handleLogin()"
-            type="primary" class="btn-login">登录
-            </el-button>
-        </el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handleLogin()" type="primary" class="btn-login">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                labelPosition: "top",
-                formdata: {
-                    username: "",
-                    password: ""
-                }
-            };
-        },
-        methods: {
-            handleLogin(){
-                // console.log(1)
-                this.$http
-                    .post(`login`, this.formdata)
-                    .then((res) => {
-                        // console.log(2)
-                        console.log(res)
-                        const{data:{data,meta:{msg, status}}} = res
-                        if( status === 200){
-                            // console.log('登录成功---')
-                            // 渲染home组件 ->改标识(js改标识)
-                            this.$router.push({
-                                name: 'home'
-                            })
-                        }else {
-                            this.$message.error(msg)
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-
-            }
-        },
+export default {
+  data() {
+    return {
+      labelPosition: "top",
+      formdata: {
+        username: "",
+        password: ""
+      }
     };
+  },
+  methods: {
+    async handleLogin() {
+      // console.log(1)
+      const res = await this.$http.post(`login`, this.formdata);
+      console.log(res);
+      const {
+        data: {
+          data:{token},
+          meta: { msg, status }
+        }
+      } = res;
+      if (status === 200) {
+
+          //把token存起来
+          localStorage.setItem("token", token);
+
+          //取值
+          localStorage.getItem("token");
+        // console.log('登录成功---')
+        // 渲染home组件 ->改标识(js改标识)
+        this.$router.push({
+          name: "home"
+        });
+      } else {
+        this.$message.error(msg);
+      }
+
+      // .then((res) => {
+      //     // console.log(2)
+      //     console.log(res)
+      //     const{data:{data,meta:{msg, status}}} = res
+      //     if( status === 200){
+      //         // console.log('登录成功---')
+      //         // 渲染home组件 ->改标识(js改标识)
+      //         this.$router.push({
+      //             name: 'home'
+      //         })
+      //     }else {
+      //         this.$message.error(msg)
+      //     }
+      // })
+      // .catch(err => {
+      //     console.log(err)
+      // })
+    }
+  }
+};
 </script>
 
 <style>
-    .login-wrap {
-        background: #324152;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .login-form {
-        background: #fff;
-        border-radius: 5px;
-        width: 400px;
-        padding: 30px;
-       
-    }
-    h2 {
-        margin: 0;
-        padding: 0;
-    }
-    .btn-login {
-        width: 100%;
-    }
+.login-wrap {
+  background: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background: #fff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+h2 {
+  margin: 0;
+  padding: 0;
+}
+.btn-login {
+  width: 100%;
+}
 </style>
 
