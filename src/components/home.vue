c1d2074d4ae5ea2822932a523dc883fdc97c2ca4
ywzj1642 <1642763705@qq.com>
2019-02-16 13:51:55 +0800

    头部样式完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,16 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-col :span="4">
+        <img src="../assets/logo.png" alt="图片加载错误">
+      </el-col>
+      <el-col :span="19" class="middle">
+        <h2>电商后台管理系统</h2>
+      </el-col>
+      <el-col :span="1">
+        <a href="#" class="logout">退出</a>
+      </el-col>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -12,16 +22,24 @@
 export default {};
 </script>
 <style>
-    .header {
-        background: #aaa;
-    }
-    .container {
-        height: 100%;
-    }
-    .aside {
-        background: pink;
-    }
-    .main {
-        background: greenyellow;
-    }
+.header {
+  background: #aaa;
+}
+.container {
+  height: 100%;
+}
+.aside {
+  background: pink;
+}
+.main {
+  background: greenyellow;
+}
+.middle {
+    line-height: 60px;
+    text-align: center;
+}
+.logout {
+    line-height: 60px;
+    text-decoration: none;
+}
 </style>
