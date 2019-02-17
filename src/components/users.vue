bfd5fc23951e156e99c28cd3095250bb14305c7f
ywzj1642 <1642763705@qq.com>
2019-02-17 13:58:42 +0800

    面包屑-搜索-添加按钮

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>users------</div>
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索框 -->
+    <div style="margin-top: 15px;">
+      <el-input placeholder="请输入内容" v-model="query" class="searchInput">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+        <el-button type="primary">添加用户</el-button>
+    </div>
+    <!-- 列表 -->
+    <!-- 分页 -->
+  </el-card>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        data(){
+            return {
+                query: ''
+            }
+        }
+    };
 </script>
 
 <style>
-
+.box {
+  height: 100%;
+}
+.searchInput {
+    width: 350px;
+}
 </style>
